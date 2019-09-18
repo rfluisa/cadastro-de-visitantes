988e5c979fb077c92c2812331a7693217efdc3c3
Thiago Pereira Correa <thiago.correa147@hotmail.com>
2019-09-18 18:58:57 -0300

    Estruturado Back End
    Adicionado WEB API, Database e toda estrutura do projeto

---
+++ backEnd/CadastroDeVisitantes/CadastroDeVisitantes/Areas/HelpPage/ModelDescriptions/IModelDocumentationProvider.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Reflection;
+
+namespace CadastroDeVisitantes.Areas.HelpPage.ModelDescriptions
+{
+    public interface IModelDocumentationProvider
+    {
+        string GetDocumentation(MemberInfo member);
+
+        string GetDocumentation(Type type);
+    }
+}
