c60f486daaa4c528d08f1aa6a8f5a99ba3f6e1be
Luisa <lu_rfa@hotmail.com>
2019-09-25 18:05:04 -0300

    adds cadastroUsuario with Post

--- js/index.js
+++ js/index.js
@@ -1,17 +1,22 @@
 function cadastrar(){
-    $.post( "https://localhost:44337/api/values/cadastro",{"cpfcnpj": $('#cpfBool').val(), 
+    $.post("https://localhost:44337/api/values/cadastro",{"cpfcnpj": $('#cpfBool').val(), 
         "cpf": $('#cpf-cnpj').val(), "nome": $('#nome').val(), "sexoM": $('#m').val(),
         "sexoF": $('#f').val(), "sexoO": $('#o').val(), "telefone": $('#telefone').val()},
-        function( data ) {$( ".result" ).html( data );});
+        function( data ){
+        $( ".result" ).html( data );
+    });
 }
 
 function login(){
-    $.post( "https://localhost:44337/api/values/login",{"usuario": $('#username').val(), 
+    $.post("https://localhost:44337/api/values/login",{"usuario": $('#username').val(), 
      "senha": $('#pwd').val()} ,function( data ) {
      $( ".result" ).html( data );
-     });
+    });
 }
 
-function registrar(){
-    
+function cadastrarUsuario(){
+    $.post("https://localhost:44337/api/values/cadastroUsuario",{"usuario": $('#username').val(), 
+     "senha": $('#pwd').val(), "tipo": $('#tipoUsuario')} ,function( data ) {
+     $( ".result" ).html( data );
+    });
 }
