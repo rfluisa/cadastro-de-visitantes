b47f155ce38b623f35031f28ec38771456de5c12
Luisa <lu_rfa@hotmail.com>
2019-11-27 19:20:46 -0300

    adds new column

--- js/styles.js
+++ js/styles.js
@@ -44,340 +44,80 @@ Highcharts.chart('pieChartVisitas', {
       }
     },
     series: [{
-      name: 'Setores',
+      name: 'Total de Visitas',
       colorByPoint: true,
       data: [{
-        name: 'Chrome',
+        name: 'Tesouraria',
         y: 61.41,
         sliced: true,
         selected: true
       }, {
-        name: 'Internet Explorer',
+        name: 'Prédio C',
         y: 11.84
       }, {
-        name: 'Firefox',
+        name: 'Cantina',
         y: 10.85
       }, {
-        name: 'Edge',
+        name: 'Diretoria',
         y: 4.67
       }, {
-        name: 'Safari',
+        name: 'Liquid Galaxy',
         y: 4.18
       }, {
-        name: 'Sogou Explorer',
+        name: 'FabLab',
         y: 1.64
       }, {
-        name: 'Opera',
+        name: 'LI',
         y: 1.6
       }, {
-        name: 'QQ',
+        name: 'Prédio L',
         y: 1.2
       }, {
-        name: 'Other',
+        name: 'Laboratório de Manufatura',
         y: 2.61
       }]
     }]
   });
 
   // Create the chart
-Highcharts.chart('container', {
-    chart: {
-      type: 'column'
-    },
+Highcharts.chart('lineChartVisitasHora', {
+  title: {
+    text: 'Fluxo de Pessoas/Hora',
+    x: -20 //center
+  },
+  xAxis: {
+    categories: ['08:00', '09:00', '10:00', '11:00', '12:00', '13:00',
+      '14:00', '15:00', '16:00', '17:00', '18:00', '19:00', '20:00', '21:00',
+      '22:00'
+    ]
+  },
+  yAxis: {
     title: {
-      text: 'Browser market shares. January, 2018'
+      text: 'Pessoas (unidade)'
     },
-    subtitle: {
-      text: 'Click the columns to view versions. Source: <a href="http://statcounter.com" target="_blank">statcounter.com</a>'
-    },
-    xAxis: {
-      type: 'category'
-    },
-    yAxis: {
-      title: {
-        text: 'Total percent market share'
-      }
-  
-    },
-    legend: {
-      enabled: false
-    },
-    plotOptions: {
-      series: {
-        borderWidth: 0,
-        dataLabels: {
-          enabled: true,
-          format: '{point.y:.1f}%'
-        }
-      }
-    },
-  
-    tooltip: {
-      headerFormat: '<span style="font-size:11px">{series.name}</span><br>',
-      pointFormat: '<span style="color:{point.color}">{point.name}</span>: <b>{point.y:.2f}%</b> of total<br/>'
-    },
-  
-    series: [
-      {
-        name: "Browsers",
-        colorByPoint: true,
-        data: [
-          {
-            name: "Chrome",
-            y: 62.74,
-            drilldown: "Chrome"
-          },
-          {
-            name: "Firefox",
-            y: 10.57,
-            drilldown: "Firefox"
-          },
-          {
-            name: "Internet Explorer",
-            y: 7.23,
-            drilldown: "Internet Explorer"
-          },
-          {
-            name: "Safari",
-            y: 5.58,
-            drilldown: "Safari"
-          },
-          {
-            name: "Edge",
-            y: 4.02,
-            drilldown: "Edge"
-          },
-          {
-            name: "Opera",
-            y: 1.92,
-            drilldown: "Opera"
-          },
-          {
-            name: "Other",
-            y: 7.62,
-            drilldown: null
-          }
-        ]
-      }
-    ],
-    drilldown: {
-      series: [
-        {
-          name: "Chrome",
-          id: "Chrome",
-          data: [
-            [
-              "v65.0",
-              0.1
-            ],
-            [
-              "v64.0",
-              1.3
-            ],
-            [
-              "v63.0",
-              53.02
-            ],
-            [
-              "v62.0",
-              1.4
-            ],
-            [
-              "v61.0",
-              0.88
-            ],
-            [
-              "v60.0",
-              0.56
-            ],
-            [
-              "v59.0",
-              0.45
-            ],
-            [
-              "v58.0",
-              0.49
-            ],
-            [
-              "v57.0",
-              0.32
-            ],
-            [
-              "v56.0",
-              0.29
-            ],
-            [
-              "v55.0",
-              0.79
-            ],
-            [
-              "v54.0",
-              0.18
-            ],
-            [
-              "v51.0",
-              0.13
-            ],
-            [
-              "v49.0",
-              2.16
-            ],
-            [
-              "v48.0",
-              0.13
-            ],
-            [
-              "v47.0",
-              0.11
-            ],
-            [
-              "v43.0",
-              0.17
-            ],
-            [
-              "v29.0",
-              0.26
-            ]
-          ]
-        },
-        {
-          name: "Firefox",
-          id: "Firefox",
-          data: [
-            [
-              "v58.0",
-              1.02
-            ],
-            [
-              "v57.0",
-              7.36
-            ],
-            [
-              "v56.0",
-              0.35
-            ],
-            [
-              "v55.0",
-              0.11
-            ],
-            [
-              "v54.0",
-              0.1
-            ],
-            [
-              "v52.0",
-              0.95
-            ],
-            [
-              "v51.0",
-              0.15
-            ],
-            [
-              "v50.0",
-              0.1
-            ],
-            [
-              "v48.0",
-              0.31
-            ],
-            [
-              "v47.0",
-              0.12
-            ]
-          ]
-        },
-        {
-          name: "Internet Explorer",
-          id: "Internet Explorer",
-          data: [
-            [
-              "v11.0",
-              6.2
-            ],
-            [
-              "v10.0",
-              0.29
-            ],
-            [
-              "v9.0",
-              0.27
-            ],
-            [
-              "v8.0",
-              0.47
-            ]
-          ]
-        },
-        {
-          name: "Safari",
-          id: "Safari",
-          data: [
-            [
-              "v11.0",
-              3.39
-            ],
-            [
-              "v10.1",
-              0.96
-            ],
-            [
-              "v10.0",
-              0.36
-            ],
-            [
-              "v9.1",
-              0.54
-            ],
-            [
-              "v9.0",
-              0.13
-            ],
-            [
-              "v5.1",
-              0.2
-            ]
-          ]
-        },
-        {
-          name: "Edge",
-          id: "Edge",
-          data: [
-            [
-              "v16",
-              2.6
-            ],
-            [
-              "v15",
-              0.92
-            ],
-            [
-              "v14",
-              0.4
-            ],
-            [
-              "v13",
-              0.1
-            ]
-          ]
-        },
-        {
-          name: "Opera",
-          id: "Opera",
-          data: [
-            [
-              "v50.0",
-              0.96
-            ],
-            [
-              "v49.0",
-              0.82
-            ],
-            [
-              "v12.1",
-              0.14
-            ]
-          ]
-        }
-      ]
-    }
-  });
+    plotLines: [{
+      value: 0,
+      width: 1,
+      color: '#808080'
+    }]
+  },
+  legend: {
+    layout: 'vertical',
+    align: 'right',
+    verticalAlign: 'middle',
+    borderWidth: 0
+  },
+  series: [{
+    name: 'Secretaria',
+    data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6, 12.0,14.0,18.9]
+  }, {
+    name: 'FabLab',
+    data: [-0.2, 0.8, 5.7, 11.3, 17.0, 22.0, 24.8, 24.1, 20.1, 14.1, 8.6, 2.5]
+  }, {
+    name: 'Tesouraria',
+    data: [-0.9, 0.6, 3.5, 8.4, 13.5, 17.0, 18.6, 17.9, 14.3, 9.0, 3.9, 1.0]
+  }, {
+    name: 'Cantina',
+    data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
+  }]
+});
