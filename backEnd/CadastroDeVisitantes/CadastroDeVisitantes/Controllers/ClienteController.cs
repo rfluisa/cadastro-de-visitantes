dceb8cfff2beefad6549dc7f8f7543ff6f3510aa
Thiago Pereira Correa <thiago.correa147@hotmail.com>
2019-12-01 17:55:40 -0300

    commit mudanças front e back

---
+++ backEnd/CadastroDeVisitantes/CadastroDeVisitantes/Controllers/ClienteController.cs
@@ -0,0 +1,25 @@
+﻿using CadastroDeVisitantes.Core;
+using CadastroDeVisitantes.Models;
+using DataModels;
+using LinqToDB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using System.Web.Http.Results;
+using System.Web.Security;
+
+namespace CadastroDeVisitantes.Controllers
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class ClienteController : ApiController
+    {
+        public void Teste()
+        {
+
+        }
+    }
+}
